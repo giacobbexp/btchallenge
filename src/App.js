846543151207c974279bc0adc27b73791420a888
giacobbexp <jakemalkin@hotmail.com>
2021-05-20 10:01:07 +0100

    Search component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 import { Header, Footer } from './Components/Core';
+import { Search } from './Components/News';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <div className="container pt-5">
+      <div className="container mt-5 p-5 section-main">
         <div className="row">
           <div className="col">
             <h1>BT React Code Test - by Jake Malkin - 19/05/2021</h1>
-            <p>body</p>
+            <Search />
           </div>
         </div>
       </div>
---
+++ src/Components/News/Search.js
@@ -0,0 +1,16 @@
+const Search = () => {
+    return (
+        <>
+            <div className="row mt-5">
+                <div className="col-sm-6 offset-sm-3">
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="What news would you like to see?" />
+                        <button className="btn btn-bt" type="button" id="button-addon2">Search</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Search;

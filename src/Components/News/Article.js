a5bcc2264b9418109b415016ddf0b18f4db1535a
giacobbexp <jakemalkin@hotmail.com>
2021-05-20 11:40:23 +0100

    Added regex for detecting if the author is a URL
    Cheers Stackoverflow :+1:

--- src/Components/News/Article.js
+++ src/Components/News/Article.js
@@ -3,6 +3,16 @@ const Article = (props) => {
         return str.length > 150 ? str.substring(0, 150) + '...' : str;
     }
 
+    const isUrl = (str) => {
+        var pattern = new RegExp('^(https?:\\/\\/)?' +
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+            '((\\d{1,3}\\.){3}\\d{1,3}))' +
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+            '(\\?[;&a-z\\d%_.~+=-]*)?' +
+            '(\\#[-a-z\\d_]*)?$', 'i');
+        return !!pattern.test(str);
+    }
+
     return (
         <>
             <div className="row article">
@@ -13,7 +23,7 @@ const Article = (props) => {
                     <a href={props.content.url} target="_blank" rel="noreferrer"><p className="text-muted">{props.content.url}</p></a>
                     <a href={props.content.url} target="_blank" rel="noreferrer"><h5 style={{ color: '#5514b4' }}><strong>{props.content.title}</strong></h5></a>
                     <p>{shortenString(props.content.content)}</p>
-                     <p className="text-muted"><small>{props.content.author && <>By {props.content.author} - </>}{props.content.publishedAt.substring(0, 10)}</small></p>
+                    <p className="text-muted"><small>{props.content.author && <>By {isUrl(props.content.author) ? <a href={props.content.author}>{props.content.author}</a> : props.content.author} - </>}{props.content.publishedAt.substring(0, 10)}</small></p>
                 </div>
             </div>
             <hr />
